3fa36ac4c0afeec69977fa385c882f006857f1b2
ABCbum <nguyennam221002@gmail.com>
2022-01-12 11:41:28 +0700

    Design changes and code warnings for Login and MainActivity

--- app/src/main/java/com/rmit/tictactoe/RegisterActivity.java
+++ app/src/main/java/com/rmit/tictactoe/RegisterActivity.java
@@ -5,17 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -24,9 +19,7 @@ import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private TextView banner;
     private EditText etFullname, etAge, etEmail, etPassword;
-    private Button register;
 
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
@@ -36,87 +29,87 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
 
-        banner = (TextView) findViewById(R.id.banner);
-        banner.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+
+        TextView banner = findViewById(R.id.banner);
+        banner.setOnClickListener(v -> finish());
+
+
+//        Button backButton = findViewById(R.id.backButton);
+//        backButton.setOnClickListener(v -> toLogin());
+
+
+        Button register = findViewById(R.id.register);
+        register.setOnClickListener(v -> {
+            etFullname = findViewById(R.id.fullName);
+            etAge = findViewById(R.id.age);
+            etEmail = findViewById(R.id.email);
+            etPassword = findViewById(R.id.password);
+
+            String fullName = etFullname.getText().toString().trim();
+            String age = etAge.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (fullName.isEmpty()) {
+                etFullname.setError("User name is required");
+                etFullname.requestFocus();
+                return;
             }
-        });
 
-        register = (Button) findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                etFullname = (EditText) findViewById(R.id.fullName);
-                etAge = (EditText) findViewById(R.id.age);
-                etEmail = (EditText) findViewById(R.id.email);
-                etPassword = (EditText) findViewById(R.id.password);
+            if (age.isEmpty()) {
+                etAge.setError("User age is required");
+                etAge.requestFocus();
+                return;
+            }
 
-                String fullName = etFullname.getText().toString().trim();
-                String age = etAge.getText().toString().trim();
-                String email = etEmail.getText().toString().trim();
-                String password = etPassword.getText().toString().trim();
+            if (email.isEmpty()) {
+                etEmail.setError("Email is required");
+                etEmail.requestFocus();
+                return;
+            }
 
-                if(fullName.isEmpty()){
-                    etFullname.setError("User name is required");
-                    etFullname.requestFocus();
-                    return;
-                }
-                if(age.isEmpty()){
-                    etAge.setError("User age is required");
-                    etAge.requestFocus();
-                    return;
-                }
-                if(email.isEmpty()){
-                    etEmail.setError("Email is required");
-                    etEmail.requestFocus();
-                    return;
-                }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                etEmail.setError("Email is invalid");
+                etEmail.requestFocus();
+                return;
+            }
 
-                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                    etEmail.setError("Email is invalid");
-                    etEmail.requestFocus();
-                    return;
-                }
+            if (password.isEmpty()) {
+                etPassword.setError("Password is required");
+                etPassword.requestFocus();
+                return;
+            }
 
-                if(password.isEmpty()){
-                    etPassword.setError("Password is required");
-                    etPassword.requestFocus();
-                    return;
-                }
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Map<String, Object> userMap = new HashMap<>();
+                            userMap.put("fullName", fullName);
+                            userMap.put("age", age);
 
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    Map<String, Object> userMap = new HashMap<>();
-                                    userMap.put("fullName", fullName);
-                                    userMap.put("age", age);
-
-                                    db.collection("users").document(email).set(userMap)
-                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                @Override
-                                                public void onSuccess(Void unused) {
-                                                    Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_LONG).show();
-                                                    finish();
-                                                }
-                                            }).addOnFailureListener(new OnFailureListener() {
+                            db.collection("users").document(email).set(userMap)
+                                    .addOnSuccessListener(unused -> {
+                                        Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_LONG).show();
+                                        finish();
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
                                             Toast.makeText(RegisterActivity.this, "User unable to connect to database", Toast.LENGTH_LONG).show();
-                                        }
-                                    });
-                                }else{
-                                    Toast.makeText(RegisterActivity.this, "User has failed to register", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-            }
+                                        }});
+                        } else{
+                            Toast.makeText(RegisterActivity.this, "User has failed to register", Toast.LENGTH_LONG).show();
+                        }
+                    });
         });
     }
+
+//    private void toLogin() {
+//        Intent intent = new Intent(this, EmailLoginActivity.class);
+//        startActivity(intent);
+//    }
 }
